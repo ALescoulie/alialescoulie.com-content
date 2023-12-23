423a34fb3ebd8aadf7c268b91a364108895d3cdc
Alia Lescoulie <alia.lescoulie@gmail.com>
2023-12-23 14:58:38 -0800

    add new flake

--- flake.nix
+++ flake.nix
@@ -1,46 +1,32 @@
 {
-  description = "Application packaged using poetry2nix";
-
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
     sitegen.url = "github:ALescoulie/sitegen";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix, sitegen }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+  outputs = {
+    systems,
+    nixpkgs,
+    sitegen,
+    ...
+  } @ inputs: let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+      );
+  in {
+    packages = eachSystem (pkgs: {
+      hello = pkgs.hello;
+    });
 
-        customOverrides = final: prev: {
-          pandoc = prev.pandoc.overrideAttrs (oldAttrs: {
-            buildInputs = [ prev.setuptools ];
-            runtimeInputs = [ prev.setuptools ];
-          });
-        };
-
-        pandoc = pkgs.legacyPackages.${system}.pandoc;
-
-      in
-      {
-
-        content-utils = mkPoetryApplication {
-          projectDir = ./.; 
-        };
-
-        packages = {
-          default = self.packages.${system}.myapp;
-        };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.myapp ];
-          packages = [ pkgs.poetry pandoc sitegen.packages.${system}.sitegen ];
-        };
-      });
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          sitegen.packages.${pkgs.system}.sitegen
+        ];
+      };
+    });
+  };
 }
